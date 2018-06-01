eb9b2ced2fbf62494197f141971a8270e1b03bf2
oscardbritto <39585840+oscardbritto@users.noreply.github.com>
2018-06-01 14:02:27 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ const client = new Discord.Client();
 client.on('ready', () => {
     console.log('I am ready!');
 });
+bot.user.setgame("clash of clans"); 
 
 client.on('message', message => {
     if (message.content === 'ping') {
