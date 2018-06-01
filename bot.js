ffd72af0921aedce528f91ce9d88079046115588
oscardbritto <39585840+oscardbritto@users.noreply.github.com>
2018-06-01 14:07:26 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,9 +3,9 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
+bot.user.setgame("clash of clans");
 });
- bot.user.setgame("clash of clans"); 
-
+  
 client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
